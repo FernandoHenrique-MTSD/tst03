f68c5c309b208093f966741f02cb3e83653dfcd5
FernandoHenrique-MTSD <158511149+FernandoHenrique-MTSD@users.noreply.github.com>
2024-02-29 15:15:45 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DLogin</title>
+</head>
+<body>
+    <h1>ACESSE SUA CONTA</h1>
+    <form action="" method="POST">
+        <label> E-MAIL <label>
+        <p>
+        <input type="text" name="email">
+        </p>
+        <label> SENHA <label>
+        <p>
+        <input type="password" name="senha">
+        </p>
+        <p>
+        <button type="submit">LOGIN</button>
+        </p>
+</form>
+</body>
+</html>
