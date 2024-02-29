55321d924e3609a52ecbee07b745aba157cdeba3
FernandoHenrique-MTSD <158511149+FernandoHenrique-MTSD@users.noreply.github.com>
2024-02-29 17:33:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
 include('conexao.php');
+
+if(isset($_POST['e-mail']) || isset($_POST['senha'])) {
+    if(strlen)($_POST['email']) == 0 {
+        echo "PREENCHA SEU E-MAIL";
+        } else if (strlen($_POST['senha']) == 0) {
+    echo "PREENCHA SUA SENHA";
+} else {
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $senha = $mysqli->real_escape_string($_POST['senha']);
+
+    $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+    $sql_query = $mysql->query($sql_code) or die("FALHA NA EXECUCAO SQL" . $mysqli->error);
+
+    $quantidade = $sql_query->num_rows;
+
+    if($quantidade == 1) {
+
+        $usuario = $sql_query->fetch_assoc();
+
+        if(!isset($_SESSION)) {
+            session_start();
+        }
+
+    $_SESSION['id'] = $usuario['id'];
+
+    header("Location: painel.php");
+        
+    } else {
+        echo "FALHA AO LOGAR ! E-MAIL OU SENHA INCORRETOS"
+    }
+}
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
